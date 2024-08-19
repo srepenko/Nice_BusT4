fedeffb7a5707f4ef3b577899ae3694e16466c86
RSV <srepenko@gmail.com>
2024-08-19 14:45:12 +0300

    1

--- components/bus_t4/__init__.py
+++ components/bus_t4/__init__.py
@@ -19,6 +19,11 @@ CONFIG_SCHEMA = (
     .extend(uart.UART_DEVICE_SCHEMA)
 )
 
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
+    
 #CONF_NICEBUST4_ID = "nicebust4_id"
 
 #bus_t4_ns = cg.esphome_ns.namespace("bus_t4")
