af9c4c66f1f41e5b5caeff9ca0d083eb4c7d6f1f
RSV <srepenko@gmail.com>
2024-08-19 09:56:55 +0300

    1

--- components/bus_t4/__init__.py
+++ components/bus_t4/__init__.py
@@ -8,3 +8,20 @@ DEPENDENCIES = ["uart"]
 CODEOWNERS = ["@srepenko"]
 MULTI_CONF = True
 
+#empty_uart_component_ns = cg.esphome_ns.namespace("empty_uart_component")
+#EmptyUARTComponent = empty_uart_component_ns.class_(
+#    "EmptyUARTComponent", cg.Component, uart.UARTDevice
+#)
+
+#CONFIG_SCHEMA = (
+#    cv.Schema({cv.GenerateID(): cv.declare_id(EmptyUARTComponent)})
+#    .extend(cv.COMPONENT_SCHEMA)
+#    .extend(uart.UART_DEVICE_SCHEMA)
+#)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
+
