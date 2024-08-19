18c83152bf2757317475f55416a44c5ee268a26d
RSV <srepenko@gmail.com>
2024-08-19 09:59:41 +0300

    1

--- components/bus_t4/__init__.py
+++ components/bus_t4/__init__.py
@@ -19,7 +19,6 @@ MULTI_CONF = True
 #    .extend(uart.UART_DEVICE_SCHEMA)
 #)
 
-
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
