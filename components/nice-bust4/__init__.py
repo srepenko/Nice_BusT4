2ca7b24a333cf03acff790e548ae2c835af8a3f5
RSV <srepenko@gmail.com>
2024-08-19 15:34:54 +0300

    1

--- components/nice-bust4/__init__.py
+++ components/nice-bust4/__init__.py
@@ -2,24 +2,17 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID
-from esphome.components import time
 
 DEPENDENCIES = ["uart"]
-AUTO_LOAD = ['uart', 'cover']
-CODEOWNERS = ["@srepenko"]
-MULTI_CONF = True
 
-nice_bust4_ns = cg.esphome_ns.namespace("nice_bust4")
-NiceBusT4Component = nice_bust4_ns.class_('NiceBusT4Component', cg.PollingComponent)
-MULTI_CONF = True
-
-NiceBusT4 = nice_bust4_ns.class_(
-    "NiceBusT4", cg.Component, uart.UARTDevice
+empty_uart_component_ns = cg.esphome_ns.namespace("empty_uart_component")
+EmptyUARTComponent = empty_uart_component_ns.class_(
+    "EmptyUARTComponent", cg.Component, uart.UARTDevice
 )
 
-CONFIG_SCHEMA = cv.All(
-    cv.Schema({cv.GenerateID(): cv.declare_id(NiceBusT4)})
-    .extend(cv.polling_component_schema("1000ms"))
+CONFIG_SCHEMA = (
+    cv.Schema({cv.GenerateID(): cv.declare_id(EmptyUARTComponent)})
+    .extend(cv.COMPONENT_SCHEMA)
     .extend(uart.UART_DEVICE_SCHEMA)
 )
 
@@ -28,4 +21,3 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
-
