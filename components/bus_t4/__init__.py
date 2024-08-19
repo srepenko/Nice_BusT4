be7e71df67155e73c4f460d0beb0021ade65e38c
RSV <srepenko@gmail.com>
2024-08-19 14:39:00 +0300

    1

--- components/bus_t4/__init__.py
+++ components/bus_t4/__init__.py
@@ -8,32 +8,32 @@ DEPENDENCIES = ["uart"]
 CODEOWNERS = ["@srepenko"]
 MULTI_CONF = True
 
-CONF_NICEBUST4_ID = "nicebust4_id"
+#CONF_NICEBUST4_ID = "nicebust4_id"
 
-nicebust4_ns = cg.esphome_ns.namespace("nicebust4")
-NiceBusT4Component = nicebust4_ns.class_("NiceBusT4", cg.Component)
+bus_t4_ns = cg.esphome_ns.namespace("bus_t4")
+#NiceBusT4Component = nicebust4_ns.class_("NiceBusT4", cg.Component)
 
-NICEBUST4_COMPONENT_SCHEMA = cv.Schema(
-    {
-        cv.Required(CONF_NICEBUST4_ID): cv.use_id(NiceBusT4Component),
-    }
-)
-
-#NiceBusT4 = nicebust4_ns.class_(
-#    "NiceBusT4", cg.Component, uart.UARTDevice
+#NICEBUST4_COMPONENT_SCHEMA = cv.Schema(
+#    {
+#        cv.Required(CONF_NICEBUST4_ID): cv.use_id(NiceBusT4Component),
+#    }
 #)
 
-CONFIG_SCHEMA = cv.All(
-    cv.Schema({cv.GenerateID(): cv.declare_id(NiceBusT4Component)})
-    .extend(cv.polling_component_schema("1000ms"))
-    .extend(uart.UART_DEVICE_SCHEMA)
+bus_t4 = bus_t4_ns.class_(
+    "bus_t4", cg.Component, uart.UARTDevice
 )
 
-#CONFIG_SCHEMA = (
-#    cv.Schema({cv.GenerateID(): cv.declare_id(NiceBusT4)})
-#    .extend(cv.COMPONENT_SCHEMA)
+#CONFIG_SCHEMA = cv.All(
+#    cv.Schema({cv.GenerateID(): cv.declare_id(NiceBusT4Component)})
+#    .extend(cv.polling_component_schema("1000ms"))
 #    .extend(uart.UART_DEVICE_SCHEMA)
 #)
+
+CONFIG_SCHEMA = (
+    cv.Schema({cv.GenerateID(): cv.declare_id(bus_t4)})
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
