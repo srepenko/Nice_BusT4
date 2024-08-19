493c89f9926f2b6695c82493a6ecd311425ca07d
RSV <srepenko@gmail.com>
2024-08-19 15:45:58 +0300

    1

--- components/bus_t4/__init__.py
+++ components/bus_t4/__init__.py
@@ -1,7 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
+from esphome.components import cover
 from esphome.const import CONF_ID
+
 
 DEPENDENCIES = ["uart"]
 
