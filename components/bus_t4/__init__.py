4f6af20d4b369da9018a398358e0da645480719a
RSV <srepenko@gmail.com>
2024-08-19 09:37:10 +0300

    1

--- components/bus_t4/__init__.py
+++ components/bus_t4/__init__.py
@@ -1,1 +1,10 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+from esphome.const import CONF_ID
+from esphome.components import time
+
+DEPENDENCIES = ["uart"]
+CODEOWNERS = ["@srepenko"]
+MULTI_CONF = True
 
