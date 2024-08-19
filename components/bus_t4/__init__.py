8126a9f122aedd349292f579c121ea4e97e329a3
RSV <srepenko@gmail.com>
2024-08-19 15:05:38 +0300

    1

--- components/bus_t4/__init__.py
+++ components/bus_t4/__init__.py
@@ -35,8 +35,8 @@ CONFIG_SCHEMA = (
     .extend(uart.UART_DEVICE_SCHEMA)
 )
 
-#async def to_code(config):
-#    var = cg.new_Pvariable(config[CONF_ID])
-#    await cg.register_component(var, config)
-#    await uart.register_uart_device(var, config)
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
 
