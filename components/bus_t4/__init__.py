0343212569c535801a0795e677183a27fbeb217e
RSV <srepenko@gmail.com>
2024-08-19 15:00:07 +0300

    1

--- components/bus_t4/__init__.py
+++ components/bus_t4/__init__.py
@@ -5,24 +5,25 @@ from esphome.const import CONF_ID
 from esphome.components import time
 
 DEPENDENCIES = ["uart"]
+AUTO_LOAD = ['uart']
 CODEOWNERS = ["@srepenko"]
 MULTI_CONF = True
 
-#bus_t4_ns = cg.esphome_ns.namespace("bus_t4")
-#Bus_T4 = bus_t4_ns.class_(
-#    "Bus_T4", cg.Component, uart.UARTDevice
-#)
+bus_t4_ns = cg.esphome_ns.namespace("bus_t4")
+Bus_T4 = bus_t4_ns.class_(
+    "Bus_T4", cg.Component, uart.UARTDevice
+)
 
-#CONFIG_SCHEMA = (
-#    cv.Schema({cv.GenerateID(): cv.declare_id(Bus_T4)})
-#    .extend(cv.COMPONENT_SCHEMA)
-#    .extend(uart.UART_DEVICE_SCHEMA)
-#)
+CONFIG_SCHEMA = (
+    cv.Schema({cv.GenerateID(): cv.declare_id(Bus_T4)})
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
 
-#async def to_code(config):
-#    var = cg.new_Pvariable(config[CONF_ID])
-#    await cg.register_component(var, config)
-#    await uart.register_uart_device(var, config)
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
 
 
 
