9e55a39451ad4fafa4ad845135a2d9aff209f6b6
RSV <srepenko@gmail.com>
2024-08-19 15:57:57 +0300

    1

--- components/bus_t4/nice-bust4.cpp
+++ components/bus_t4/nice-bust4.cpp
@@ -118,7 +118,7 @@ void NiceBusT4::loop() {
   //while (uart_rx_available(_uart) > 0) {
   while (this->available() > 0) {
     uint8_t c = 0;//(uint8_t)uart_read_char(_uart);                // считываем байт
-    this->read_bytes(c, 1);
+    this->read_array(c, 1);
     this->handle_char_(c);                                     // отправляем байт на обработку
     this->last_uart_byte_ = now;
   } //while
