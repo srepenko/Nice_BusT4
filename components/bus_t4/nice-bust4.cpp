586c58033133e104af1b9afbd37ca56e75799b7b
RSV <srepenko@gmail.com>
2024-08-19 11:10:37 +0300

    1

--- components/bus_t4/nice-bust4.cpp
+++ components/bus_t4/nice-bust4.cpp
@@ -68,7 +68,7 @@ void NiceBusT4::setup() {
   ////_uart =  uart_init(_UART_NO, BAUD_WORK, SERIAL_8N1, SERIAL_FULL, TX_P, 256, false);
   // кто в сети?
 //  this->tx_buffer_.push(gen_inf_cmd(0x00, 0xff, FOR_ALL, WHO, GET, 0x00));
-  
+  ESP_LOGW(TAG, "Setup");
 }
 
 void NiceBusT4::loop() {
