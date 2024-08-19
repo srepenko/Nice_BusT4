3042079d710e38346eba687c93ee19776a39b2de
RSV <srepenko@gmail.com>
2024-08-19 15:04:35 +0300

    1

--- components/bus_t4/__init__.py
+++ components/bus_t4/__init__.py
@@ -9,29 +9,9 @@ AUTO_LOAD = ['uart', 'cover']
 CODEOWNERS = ["@srepenko"]
 MULTI_CONF = True
 
-bus_t4_ns = cg.esphome_ns.namespace("bus_t4")
-Bus_T4 = bus_t4_ns.class_(
-    "Bus_T4", cg.Component, uart.UARTDevice
-)
-
-CONFIG_SCHEMA = (
-    cv.Schema({cv.GenerateID(): cv.declare_id(Bus_T4)})
-    .extend(cv.COMPONENT_SCHEMA)
-    .extend(uart.UART_DEVICE_SCHEMA)
-)
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
-
-
-
-
-
 #CONF_NICEBUST4_ID = "nicebust4_id"
 
-#bus_t4_ns = cg.esphome_ns.namespace("bus_t4")
+bus_t4_ns = cg.esphome_ns.namespace("bus_t4")
 
 #NICEBUST4_COMPONENT_SCHEMA = cv.Schema(
 #    {
