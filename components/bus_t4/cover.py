0f500dada740ada9afaee7e1a7334efb4e30dfb1
RSV <srepenko@gmail.com>
2024-08-19 16:12:43 +0300

    1

--- components/bus_t4/cover.py
+++ components/bus_t4/cover.py
@@ -20,7 +20,8 @@ CONFIG_SCHEMA = cover.COVER_SCHEMA.extend({
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
-
+    yield uart.register_uart_device(var, config)
+    
     yield cover.register_cover(var, config)
 
     if CONF_ADDRESS in config:
