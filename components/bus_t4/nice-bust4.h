8726a88e6231fae4d65772ccd9ba133728f5fc5a
RSV <srepenko@gmail.com>
2024-08-20 08:36:28 +0300

    1

--- components/bus_t4/nice-bust4.h
+++ components/bus_t4/nice-bust4.h
@@ -370,7 +370,7 @@ struct packet_rsp_body_t {
 
 
 // создаю класс, наследую членов классов Component и Cover
-class NiceBusT4 : public Component, public Cover, public uart::IDFUARTComponent{
+class NiceBusT4 : public Component, public Cover, public IDFUARTComponent{
 //class NiceBusT4 : public uart::UARTDevice, public Component, public Cover {
   public:
 	
