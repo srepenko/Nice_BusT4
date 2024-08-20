259f7bbf35bf2b5fbad45a2820f44e61d1358a90
RSV <srepenko@gmail.com>
2024-08-20 13:55:23 +0300

    1

--- components/bus_t4/nice-bust4.cpp
+++ components/bus_t4/nice-bust4.cpp
@@ -85,8 +85,7 @@ void NiceBusT4::loop() {
         if (this->init_ok == false) {
           this->tx_buffer_.push(gen_inf_cmd(0x00, 0xff, FOR_ALL, WHO, GET, 0x00));
           this->tx_buffer_.push(gen_inf_cmd(0x00, 0xff, FOR_ALL, PRD, GET, 0x00)); //запрос продукта
-        }
-        
+        }        
         else if (this->class_gate_ == 0x55) init_device((uint8_t)(this->to_addr >> 8), (uint8_t)(this->to_addr & 0xFF), 0x04);  
         else if (this->manufacturer_ == unknown)  {
          init_device((uint8_t)(this->to_addr >> 8), (uint8_t)(this->to_addr & 0xFF), 0x04);  
