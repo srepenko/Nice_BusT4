1ee7543c6341169859c64b96b3d69973b9b9193f
RSV <srepenko@gmail.com>
2024-08-20 13:39:45 +0300

    1

--- components/bus_t4/nice-bust4.cpp
+++ components/bus_t4/nice-bust4.cpp
@@ -136,11 +136,11 @@ bool NiceBusT4::validate_message_() {                    // проверка п�
   uint8_t *data = &this->rx_message_[0];               // указатель на первый байт сообщения
   uint8_t new_byte = data[at];                      // последний полученный байт
   // Byte 0: HEADER1 (всегда 0x00)
-  if (at == 0xE0)
-    return new_byte == 0x00;
+  //if (at == 0x00)
+   // return new_byte == 0x00;
   // Byte 1: HEADER2 (всегда 0x55)
-  if (at == 1)
-    return new_byte == START_CODE;
+  //if (at == 1)
+//    return new_byte == START_CODE;
 
   // Byte 2: packet_size - количество байт дальше + 1
   // Проверка не проводится
