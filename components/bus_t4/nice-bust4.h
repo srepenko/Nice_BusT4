e482bc8a1bd02660f6ed24be1fe89b28967de2da
RSV <srepenko@gmail.com>
2024-08-22 20:32:28 +0300

    1

--- components/bus_t4/nice-bust4.h
+++ components/bus_t4/nice-bust4.h
@@ -371,7 +371,7 @@ struct packet_rsp_body_t {
 #define LIN_BREAK_BAUDRATE(BAUD) ((BAUD * 9) / 13)
 
 // создаю класс, наследую членов классов Component и Cover
-class NiceBusT4 :  public uart::IDFUARTComponent, public Component{
+class NiceBusT4 :  public uart::IDFUARTComponent{
 //class NiceBusT4 : public Component, public Cover {
   public:
 	
